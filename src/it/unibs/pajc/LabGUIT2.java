d02c780d04ebcf2bcd4c29310f93526e80e04612
Samuele Valperta <samuele.valperta@gmail.com>
2023-10-12 15:32:37 +0200

    Refactor UI and extend the functionality in LabGUIT2
    Adjusted the layout of LabGUIT2 form, switching from a GridLayoutManager to a BorderLayout, and replaced the JSlider with a JProgressBar. Furthermore, added a JComboBox for color selection to the form.
    In the LabGUIT2 class, introduced an event listener to the JSlider that sets the value to JProgressBar. Also, created a HashMap for color options and used it in filling the JComboBox with color options. Added an event listener to JComboBox to display the selected color's name in the lblInfo and changing lblInfo's text color. Changes aimed at enhancing the user experience and providing additional functionalities. Not Working at this stage.

--- src/it/unibs/pajc/LabGUIT2.java
+++ src/it/unibs/pajc/LabGUIT2.java
@@ -1,13 +1,17 @@
 package it.unibs.pajc;
 
 import javax.swing.*;
-import java.awt.EventQueue;
+import java.awt.*;
+import java.util.HashMap;
+import java.util.Map;
 
 public class LabGUIT2 extends JFrame {
 
     private JPanel MainPanel;
     private JLabel lblInfo;
-    private JSlider slider1;
+    private JSlider slider;
+    private JProgressBar progressBar;
+    private JComboBox comboBoxColors;
 
     public LabGUIT2() {
         setTitle("Title");
@@ -15,6 +19,32 @@ public class LabGUIT2 extends JFrame {
         setSize(300, 200);
         setLocationRelativeTo(null);
         add(MainPanel);
+
+        /*slider.addChangeListener(e -> {
+            lblInfo.setText(String.valueOf(slider.getValue()));
+        });*/
+
+        slider.addChangeListener(e -> {
+            progressBar.setValue(slider.getValue());
+        });
+
+        HashMap<String, Color> colorMap = new HashMap<>();
+        colorMap.put("Red", Color.RED);
+        colorMap.put("Green", Color.GREEN);
+        colorMap.put("Blue", Color.BLUE);
+        colorMap.put("Yellow", Color.YELLOW);
+
+        for (HashMap.Entry<String, Color> entry:
+             colorMap.entrySet()) {
+            comboBoxColors.addItem(entry.getKey());
+        }
+
+        comboBoxColors.addActionListener(e -> {
+                Object selectedItem = comboBoxColors.getSelectedItem();
+                lblInfo.setText(selectedItem.getKey().toString());
+                lblInfo.setForeground(selectedItem.getValue());
+        });
+
     }
 
     public static void main(String[] args) {
